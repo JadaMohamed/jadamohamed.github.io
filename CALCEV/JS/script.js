3850fea1f79387c01d95cfabce0f2d005f09ab82
JADA Mohamed <104516659+JadaMohamed@users.noreply.github.com>
2022-06-01 13:52:16 +0100

    update

---
+++ CALCEV/JS/script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    let bar =document.querySelector('.navigationbar');
+    let toggle =document.querySelector('.toggle');
+    bar.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
