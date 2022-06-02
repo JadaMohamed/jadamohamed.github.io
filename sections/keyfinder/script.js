b3e2568406cd5e78c5899c12e2cee4cfef5ae10b
JADA Mohamed <104516659+JadaMohamed@users.noreply.github.com>
2022-06-02 20:11:06 +0100

    update

--- sections/keyfinder/script.js
+++ sections/keyfinder/script.js
@@ -199,3 +199,41 @@ function toggleMenu(){
     bar.classList.toggle('active');
     toggle.classList.toggle('active');
 }
+if (localStorage.getItem('light-mode')===null){
+    localStorage.setItem('light-mode', "false");
+}
+
+checkStatus()
+
+function checkStatus(){
+    var elem= document.getElementById('DL');
+    if (localStorage.getItem('light-mode')==="true"){
+        document.body.classList.add('light-mode');
+        elem.src="images/moon.png";
+    }else{
+        document.body.classList.remove('light-mode');
+        elem.src="images/sun.png" ;
+    }
+}
+
+function DL(){
+    var elem= document.getElementById('DL');
+     document.body.classList.toggle('light-mode');
+    if(document.body.classList.contains('light-mode')){
+        elem.src="images/moon.png";
+    }
+     else{
+         elem.src="images/sun.png" ;
+     }
+ }
+
+
+function changeStatus(){                                           
+    if (localStorage.getItem('light-mode')==="true"){
+        localStorage.setItem('light-mode', "false");
+        DL();
+    } else{
+        localStorage.setItem('light-mode', "true");
+        DL();
+    }
+}
